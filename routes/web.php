b79c8c5de6aa1f40f077abfacdc6988a92ed4950
virtu-alex <alessio.cordari@gmail.com>
2022-09-14 14:30:46 +0200

    Hello World push

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/greeting', function () {
+    return '<h1>Hello World</h1>';
 });
+
